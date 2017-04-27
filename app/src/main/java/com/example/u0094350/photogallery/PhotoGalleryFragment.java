3f573e3c6da1c0bb680557971e069b34549b313b
ErcCC <scen01@gmail.com>
2017-04-27 11:15:03 -0400

    changed form text view to image view

--- app/src/main/java/com/example/u0094350/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/u0094350/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.u0094350.photogallery;
 
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,12 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import com.example.u0094350.photogallery.Util.EndlessRecyclerViewScrollListener;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -158,16 +157,19 @@ public class PhotoGalleryFragment extends Fragment{
 
 
     private class PhotoHolder extends RecyclerView.ViewHolder{
-        private TextView mTitleTextView;
+        private ImageView mItemImageView;
 
         public PhotoHolder(View itemView){
             super(itemView);
 
-            mTitleTextView = (TextView) itemView;
+            mItemImageView = (ImageView) itemView.findViewById(R.id.item_image_view);
         }
 
-        public void bindGalleryItem(GalleryItem item){
-            mTitleTextView.setText(item.toString());
+//        public void bindGalleryItem(GalleryItem item){
+//            mTitleTextView.setText(item.toString());
+//        }
+        public void bindDrawable(Drawable drawable){
+            mItemImageView.setImageDrawable(drawable);
         }
     }
 
@@ -180,15 +182,21 @@ public class PhotoGalleryFragment extends Fragment{
 
         @Override
         public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            TextView textView = new TextView(getActivity());
+//            TextView textView = new TextView(getActivity());
+//
+//            return new PhotoHolder(textView);
 
-            return new PhotoHolder(textView);
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.list_item_gallery, parent, false);
+            return new PhotoHolder(view);
         }
 
         @Override
         public void onBindViewHolder(PhotoHolder photoHolder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
-            photoHolder.bindGalleryItem(galleryItem);
+//            photoHolder.bindGalleryItem(galleryItem);
+            Drawable placeholder = getResources().getDrawable(R.drawable.koala_small);
+            photoHolder.bindDrawable(placeholder);
         }
 
         @Override
